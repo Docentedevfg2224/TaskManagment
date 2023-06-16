39a9352d52b2ec1756cea0386553ebaa2409b6f4
DESKTOP-U3LC303\AAAA <p>
2023-06-16 16:45:23 +0200

    insert task-view, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TaskViewComponent } from './task-view/task-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TaskViewComponent
   ],
   imports: [
     BrowserModule

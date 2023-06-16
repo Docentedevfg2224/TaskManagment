25f38c897e4102fd06ed837512d5984c40b5e131
DESKTOP-N4D8EK2\aaaaaa <ggiustino@studenti.apuliadigitalmaker.it>
2023-06-16 11:46:27 +0200

    primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskViewComponent } from './task-view/task-view.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskViewComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
